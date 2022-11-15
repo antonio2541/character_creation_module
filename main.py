53ac9ec7828a73e2c98780fa1c6489a3f6330777
Антон Межин <antonio2541@ya.ru>
2022-11-15 13:05:25 +0300

    Code Style

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from random import randint
+from graphic_arts.start_game_banner import run_screensaver
 
 
 def attack(char_name: str, char_class: str) -> str:
+    """Make damage."""
     if char_class == 'warrior':
         return (f'{char_name}; '
                 f'нанёс урон противнику равный; '
@@ -17,6 +19,7 @@ def attack(char_name: str, char_class: str) -> str:
 
 
 def defence(char_name: str, char_class: str) -> str:
+    """Strike bloking."""
     if char_class == 'warrior':
         return (f'{char_name}; '
                 f'блокировал {10 + randint(5, 10)} урона')
@@ -29,6 +32,7 @@ def defence(char_name: str, char_class: str) -> str:
 
 
 def special(char_name: str, char_class: str) -> str:
+    """Apply skill."""
     if char_class == 'warrior':
         return (f'{char_name}; '
                 f'применил специальное умение «Выносливость; '
@@ -44,6 +48,7 @@ def special(char_name: str, char_class: str) -> str:
 
 
 def start_training(char_name: str, char_class: str) -> str:
+    """Game."""
     if char_class == 'warrior':
         print(f'{char_name}, ты Воитель — отличный боец ближнего боя.')
     if char_class == 'mage':
@@ -69,6 +74,7 @@ def start_training(char_name: str, char_class: str) -> str:
 
 
 def choice_char_class() -> str:
+    """Character selection."""
     approve_choice: str = None
     char_class: str = None
     while approve_choice != 'y':
@@ -93,7 +99,8 @@ def choice_char_class() -> str:
     return char_class
 
 
-def main():
+if __name__ == '__main__':
+    run_screensaver()
     print('Приветствую тебя, искатель приключений!')
     print('Прежде чем начать игру...')
     char_name: str = input('...назови себя: ')
